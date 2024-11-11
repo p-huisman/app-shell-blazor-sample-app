c053934874e14e80df5ae9ee1417817b84954246
Peter Huisman <peter.huisman@pggm.nl>
2024-11-11 15:54:57 +0100

    trigger gh pages

--- src/CatalogApp/Program.cs
+++ src/CatalogApp/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using CatalogApp;
 using CatalogApp.Models;
 using Microsoft.FluentUI.AspNetCore.Components;
-
+//
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.RegisterCustomElement<App>("my-app");
